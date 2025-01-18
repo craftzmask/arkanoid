0b4a66911103f0db00120d3a7c8b22f731dae616
Khanh Chung <khanh.d.chung@gmail.com>
2025-01-17 22:44:31 -0800

    added Ball

---
+++ Engine/Ball.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Vec2.h"
+#include "Graphics.h"
+#include "SpriteCodex.h"
+
+class Ball
+{
+public:
+	Ball() = default;
+	Ball(const Vec2& pos_in, const Vec2& vel_in);
+	void Draw(Graphics& gfx) const;
+private:
+	static constexpr float radius = 7.0f;
+	Vec2 pos;
+	Vec2 vel;
+};
